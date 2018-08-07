c4f58d5e6b5cfc9d90661058416db271af0aa50c
liverbool <nukboon@gmail.com>
2018-08-07 19:12:21 +0700

    ignore faltal error

--- src/Messenger/Transport/AmqpSpoolTransport.php
+++ src/Messenger/Transport/AmqpSpoolTransport.php
@@ -4,15 +4,31 @@ declare(strict_types=1);
 
 namespace Chang\Messenger\Transport;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Transport\AmqpExt\AmqpTransport;
+use Symfony\Component\Messenger\Transport\AmqpExt\Connection;
+use Symfony\Component\Messenger\Transport\Serialization\DecoderInterface;
+use Symfony\Component\Messenger\Transport\Serialization\EncoderInterface;
 
 class AmqpSpoolTransport extends AmqpTransport implements AmqpSpoolTransportInterface
 {
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @var Envelope[]
      */
     private $messages = [];
+
+    public function __construct(EncoderInterface $encoder, DecoderInterface $decoder, Connection $connection, LoggerInterface $logger = null)
+    {
+        parent::__construct($encoder, $decoder, $connection);
+
+        $this->logger = $logger;
+    }
 
     /**
      * {@inheritdoc}
@@ -28,7 +44,11 @@ class AmqpSpoolTransport extends AmqpTransport implements AmqpSpoolTransportInte
     public function flush(): void
     {
         foreach ($this->messages as $message) {
-            parent::send($message);
+            try {
+                parent::send($message);
+            } catch (\AMQPException $e) {
+                $this->logger && $this->logger->error($e->getMessage());
+            }
         }
 
         $this->messages = [];
