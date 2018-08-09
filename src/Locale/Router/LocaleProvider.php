da70208bb4eea0d09f911f82f5efebd3cbf32baf
liverbool <nukboon@gmail.com>
2018-08-09 23:18:52 +0700

    dont error for route - cache:clear when no connection

---
+++ src/Locale/Router/LocaleProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chang\Locale\Router;
+
+use Sylius\Component\Locale\Provider\LocaleProviderInterface;
+
+class LocaleProvider implements LocaleProviderInterface
+{
+    /**
+     * @var LocaleProviderInterface
+     */
+    private $provider;
+
+    /**
+     * @var string
+     */
+    private $defaultLocaleCode;
+
+    public function __construct(LocaleProviderInterface $provider, string $defaultLocaleCode)
+    {
+        $this->provider = $provider;
+        $this->defaultLocaleCode = $defaultLocaleCode;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAvailableLocalesCodes(): array
+    {
+        try {
+            return $this->provider->getAvailableLocalesCodes();
+        } catch (\Exception $e) {
+            return [$this->defaultLocaleCode];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultLocaleCode(): string
+    {
+        try {
+            return $this->provider->getDefaultLocaleCode();
+        } catch (\Exception $e) {
+            return $this->defaultLocaleCode;
+        }
+    }
+}
