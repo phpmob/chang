007951d1ec636e6d612d8f221c6c607e81043a91
liverbool <nukboon@gmail.com>
2018-10-01 17:59:39 +0700

    add switch tag for twig

---
+++ src/Twig/Tag/SwitchNode.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chang\Twig\Tag;
+
+class SwitchNode extends \Twig_Node
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function compile(\Twig_Compiler $compiler)
+    {
+        $compiler
+            ->addDebugInfo($this)
+            ->write('switch (')
+            ->subcompile($this->getNode('value'))
+            ->raw(") {\n")
+            ->indent();
+
+        foreach ($this->getNode('cases') as $case) {
+            /** @var \Twig_Node $case */
+            // The 'body' node may have been removed by Twig if it was an empty text node in a sub-template,
+            // outside of any blocks
+            if (!$case->hasNode('body')) {
+                continue;
+            }
+
+            foreach ($case->getNode('values') as $value) {
+                $compiler
+                    ->write('case ')
+                    ->subcompile($value)
+                    ->raw(":\n");
+            }
+
+            $compiler
+                ->write("{\n")
+                ->indent()
+                ->subcompile($case->getNode('body'))
+                ->write("break;\n")
+                ->outdent()
+                ->write("}\n");
+        }
+
+        if ($this->hasNode('default')) {
+            $compiler
+                ->write("default:\n")
+                ->write("{\n")
+                ->indent()
+                ->subcompile($this->getNode('default'))
+                ->outdent()
+                ->write("}\n");
+        }
+
+        $compiler
+            ->outdent()
+            ->write("}\n");
+    }
+}
